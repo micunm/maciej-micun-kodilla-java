d4d62fe1fbd57c093629fdb62b4cc9d45c5d146b
micunm <micun@o2.pl>
2018-05-09 22:47:49 +0200

    Moduł 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator simpleCalc=new Calculator();
+
+        int sumResult=simpleCalc.add(12, 8);
+
+        if (sumResult==20) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("test NOK");
+        }
+
+        int subResult=simpleCalc.substract(12, 7);
+
+        if (subResult==5) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("test NOK");
+        }
+
+
     }
 }
