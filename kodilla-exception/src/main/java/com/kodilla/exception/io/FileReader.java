4099166a4e9bc758acbf4736e18cd79d24a6f300
micunm <micun@o2.pl>
2018-05-26 23:48:01 +0200

    task 8.2 try_catch

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,6 +1,7 @@
 package com.kodilla.exception.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,8 +11,14 @@ public class FileReader {
     public void readFile() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file=new File(classLoader.getResource("file/names.txt").getFile());
-        Path path=Paths.get(file.getPath());;
-        Stream<String> fileLines= Files.lines(path)
+      //  Path path=Paths.get(file.getPath());
+        Path path=Paths.get(file.getPath());
+
+        try {
+            Stream<String> fileLines= Files.lines(path);
+        } catch (IOException e) {
+            System.out.println("Oh no! Something wen wrong@ We will die!");
+        }
 
         System.out.println(file.getPath());
     }
