2cb9f766145d82c12af24a44946302a3446bbc02
micunm <micun@o2.pl>
2018-05-29 20:16:03 +0200

    task 8.4 exceptions exercise modification

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsExceptionHandling.java
@@ -1,4 +1,21 @@
 package com.kodilla.exception.test;
 
 public class FlightsExceptionHandling {
+    public static void main(String args[]) {
+        Flight flight1=new Flight("Warsaw", "Mexico");
+        Flight flight2=new Flight("Gdansk", "New York");
+        Flight flight3=new Flight("Gdansk", "Porto");
+
+        FlightsSearcher flightSearcher=new FlightsSearcher();
+        String isFlightFoundText="No flights to given destination.";
+
+        try {
+            System.out.println("Searching for flights..");
+            isFlightFoundText=flightSearcher.findFlight(flight2);
+        } catch (Exception x) {
+            System.out.println("Try with different destination.");
+        } finally {
+            System.out.println(isFlightFoundText);
+        }
+    }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsSearcher.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsSearcher.java
@@ -6,19 +6,23 @@ import java.util.Map;
 public class FlightsSearcher {
 
     public String findFlight(Flight flight) throws RouteNotFoundException {
-        Map<String, boolean> flightsMap=new HashMap<>();
-        flightsMap.put("Porto", 'true');
-        flightsMap.put("Gran Canaria", 'true');
-        flightsMap.put("Warsaw", 'true');
-        flightsMap.put("Sydney", 'false');
-        flightsMap.put("New Jersey", 'false');
-        flightsMap.put("Poznan", 'true');
 
+        Map<String, Boolean> flightsMap=new HashMap<>();
+        String returnString="Flight not found.";
+
+        flightsMap.put("Porto",true);
+        flightsMap.put("Gran Canaria", true);
+        flightsMap.put("Warsaw", true);
+        flightsMap.put("Sydney", false);
+        flightsMap.put("New Jersey", false);
+        flightsMap.put("Poznan", true);
 
         boolean searchResult = flightsMap.get(flight.getArrivalAirport());
-        if (searchResult=='false') {
+        if (searchResult==false) {
             throw new RouteNotFoundException();
+        } else {
+            returnString="Flight founded!";
         }
-        return "Flight founded!"
+        return returnString;
     }
 }
