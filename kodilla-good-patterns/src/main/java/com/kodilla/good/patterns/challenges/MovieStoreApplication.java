fc4d19d5788d97fc57bdc8e4627f29c50d6c87ea
micunm <micun@o2.pl>
2018-06-01 23:29:12 +0200

    task 9.1 exercise with Bond.

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreApplication.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreApplication.java
@@ -1,7 +1,15 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.lang.*;
+
 public class MovieStoreApplication {
-    MovieStore movieStore=new MovieStore();
 
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
 
+        String movieString=movieStore.getMovies().entrySet().stream()
+               // .map(entry -> entry.getKey() + "! "+entry.getValue()+" ! ")
+                .map(entry -> entry.getValue()+" ! ")
+                .forEach(System.out::println);
+    }
 }
