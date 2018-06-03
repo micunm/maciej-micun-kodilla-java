a6cf8a26aa32e8dcbb50bd89dbf2d8b5e9f41a8f
micunm <micun@o2.pl>
2018-06-03 21:27:48 +0200

    task 9.1 stream collect

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreApplication.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreApplication.java
@@ -1,15 +1,17 @@
 package com.kodilla.good.patterns.challenges;
 
 import java.lang.*;
+import java.util.stream.Collectors;
 
 public class MovieStoreApplication {
 
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
 
-        String movieString=movieStore.getMovies().entrySet().stream()
-               // .map(entry -> entry.getKey() + "! "+entry.getValue()+" ! ")
-                .map(entry -> entry.getValue()+" ! ")
-                .forEach(System.out::println);
+        String moviesString=movieStore.getMovies().entrySet().stream()
+                .map(entry -> entry.getValue().toString())
+                .collect(Collectors.joining("! "));
+
+        System.out.println(moviesString);
     }
 }
