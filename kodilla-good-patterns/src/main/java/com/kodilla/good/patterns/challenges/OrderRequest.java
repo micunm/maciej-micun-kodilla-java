beaff9e98b83a14a0ba6a12fed7f5da804697833
micunm <micun@o2.pl>
2018-06-05 05:26:13 +0200

    task 9.2 product orders service

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequest.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequest.java
@@ -4,8 +4,25 @@ import java.time.LocalDateTime;
 
 public class OrderRequest {
 
+    private User user;
+    private UserOrder userOrder;
+    private LocalDateTime orderDate;
 
+    public OrderRequest(User user, UserOrder userOrder, LocalDateTime orderDate) {
+        this.user = user;
+        this.userOrder = userOrder;
+        this.orderDate = orderDate;
+    }
 
+    public User getUser() {
+        return user;
+    }
 
+    public UserOrder getUserOrder() {
+        return userOrder;
+    }
 
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
 }
