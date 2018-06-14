841266aa2cceb7d083da017d34123988f1c83f3d
micunm <micun@o2.pl>
2018-06-14 22:22:31 +0200

    Task 10.2 mod

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double addResult=calculator.add(7, 5);
+        double subResult=calculator.sub(7, 5);
+        double mulResult=calculator.mul(12.5, 4);
+        double divResult=calculator.div(12.5, 5);
+        //Then
+        Assert.assertEquals(12, addResult, 0.00001);
+        Assert.assertEquals(2, subResult, 0.00001);
+        Assert.assertEquals(50, mulResult, 0.00001);
+        Assert.assertEquals(2.5, divResult, 0.00001);
+    }
+}
