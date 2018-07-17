c93b9a1bedf644360e14b5d257e19e17646f0cff
micunm <micun@o2.pl>
2018-07-17 16:43:32 +0200

    Task 13.4 Hibernate named Queries

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -10,4 +10,16 @@ import java.util.List;
 @Repository
 public interface TaskDao extends CrudRepository<Task, Integer> {
     List<Task> findByDuration(int duration);
+
+    @Query
+    List<Task> retrieveLongTasks();
+
+    @Query
+    List<Task> retrieveShortTasks();
+
+    @Query (nativeQuery=true)
+    List<Task> retrieveTasksWithEnoughTime();
+
+    @Query
+    List<Task> retrieveTasksWithDurationLongerThan(@Param("DURATION") int duration);
 }
