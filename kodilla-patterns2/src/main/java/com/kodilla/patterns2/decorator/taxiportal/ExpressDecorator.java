93f6de1f3ff088060605fdd179e6f7addea55419
micunm <micun@o2.pl>
2018-09-24 22:06:35 +0200

    Task 20.2 decoraotr design pattern mod2

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/decorator/taxiportal/ExpressDecorator.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/decorator/taxiportal/ExpressDecorator.java
@@ -2,17 +2,17 @@ package com.kodilla.patterns2.decorator.taxiportal;
 
 import java.math.BigDecimal;
 
-public class ChildSeatDecorator extends AbstractTaxiOrderDecorator {
-    public ChildSeatDecorator(TaxiOrder taxiOrder) {
+public class ExpressDecorator extends AbstractTaxiOrderDecorator{
+    public ExpressDecorator(TaxiOrder taxiOrder) {
         super(taxiOrder);
     }
     @Override
     public BigDecimal getCost() {
         //hardcoded stub cost = 20
-        return super.getCost().add(new BigDecimal(2));
+        return super.getCost().add(new BigDecimal(5));
     }
     @Override
     public String getDescription() {
-        return super.getDescription() + " + child seat";
+        return super.getDescription() + " express service";
     }
 }
