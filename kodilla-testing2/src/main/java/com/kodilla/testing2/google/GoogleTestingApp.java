e5fed0290df541ccbf1d34f7131491dd0b612a88
micunm <micun@o2.pl>
2018-10-10 20:10:21 +0200

    Task 23.2 Selenium Webdriver

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -0,0 +1,8 @@
+package com.kodilla.testing2.google;
+
+public class GoogleTestingApp {
+    public static void main(String[] args) {
+        System.setProperty();
+        System.setProperty();
+    }
+}
