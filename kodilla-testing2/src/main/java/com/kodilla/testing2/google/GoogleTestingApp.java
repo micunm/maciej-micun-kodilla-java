f56d5ea2c3b22d6ddb0ed50c5ab3d009be4f6d0a
micunm <micun@o2.pl>
2018-10-11 08:28:22 +0200

    Task 23.2 Selenium Webdriver mod 2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,8 +1,16 @@
 package com.kodilla.testing2.google;
 
+import com.kodilla.testing2.google.com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class GoogleTestingApp {
+    public static final String SEARCHFILE = "lst-ib";
     public static void main(String[] args) {
-        System.setProperty();
-        System.setProperty();
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("http://www.google.com");
+        WebElement searchFiled = driver.findElement(By.id(SEARCHFILE));
+        searchFiled.sendKeys("Kodilla");
     }
 }
